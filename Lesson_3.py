aa96b9114b6e0cd8e3a4758c2950eb1068fbb856
Aleksei Pivovarov <pivovarov.live@yandex.com>
2022-11-23 17:45:08 +0300

    Update Lesson_3.py

--- Lesson_3.py
+++ Lesson_3.py
@@ -385,3 +385,77 @@ for i in range(1, len(lst_in)):
     obj_new = ListObject(lst_in[i])
     obj.link(obj_new)
     obj = obj_new
+
+
+''' Объявите два класса: 
+
+Cell - для представления клетки игрового поля;
+GamePole - для управления игровым полем, размером N x N клеток.
+
+С помощью класса Cell предполагается создавать отдельные клетки командой:
+
+c1 = Cell(around_mines, mine)
+Здесь around_mines - число мин вокруг данной клетки поля; mine - булева величина (True/False), означающая наличие мины в текущей клетке. При этом, в каждом объекте класса Cell должны создаваться локальные свойства:
+
+around_mines - число мин вокруг клетки (начальное значение 0);
+mine - наличие мины в текущей клетке (True/False);
+fl_open - открыта/закрыта клетка - булево значение (True/False). Изначально все клетки закрыты (False).
+
+
+С помощью класса GamePole должна быть возможность создавать квадратное игровое поле с числом клеток N x N:
+
+pole_game = GamePole(N, M)
+Здесь N - размер поля; M - общее число мин на поле. При этом, каждая клетка представляется объектом класса Cell и все объекты хранятся в двумерном списке N x N элементов - локальном свойстве pole объекта класса GamePole. 
+
+В классе GamePole должны быть также реализованы следующие методы:
+
+init() - инициализация поля с новой расстановкой M мин (случайным образом по игровому полю, разумеется каждая мина должна находиться в отдельной клетке).
+show() - отображение поля в консоли в виде таблицы чисел открытых клеток (если клетка не открыта, то отображается символ #).
+
+При создании экземпляра класса GamePole в его инициализаторе следует вызывать метод init() для первоначальной инициализации игрового поля.
+
+В классе GamePole могут быть и другие вспомогательные методы.
+
+Создайте экземпляр pole_game класса GamePole с размером поля N = 10 и числом мин M = 12. 
+
+'''
+
+
+import random
+class Cell:
+    def __init__(self, around_mines = 0, mine = False, fl_open = False):
+        self.around_mines = around_mines
+        self.mine = mine
+        self.fl_open = fl_open
+    
+class GamePole:
+    def __init__(self, N, M):
+        self.pole = [[Cell() for _ in range(N)] for _ in range(N)]
+        self.mines = M
+
+
+    def init(self):
+        k = 0
+        q = len(self.pole)
+        while k < self.mines:
+            x, y = random.randint(0, q-1), random.randint(0, q-1)
+            if not self.pole[x][y].mine :
+                self.pole[x][y].mine = True
+                k += 1
+        
+        for x in range(q):
+            for y in range(q):
+                if not self.pole[x][y].mine:
+                    indx = (-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)
+                    mines = sum((self.pole[x+i][y+j].mine for i, j in indx if 0 <= x+i < q and 0 <= y+j < q))
+                    self.pole[x][y].around_mines = mines
+    
+    def show(self):            
+        for x in range(len(self.pole)):
+            for y in range(len(self.pole)):           
+                print('#' if self.pole[x][y].fl_open == False else '*' if self.pole[x][y].mine == True else self.pole[x][y].around_mines, end=' ')
+            print()          
+
+
+pole_game = GamePole(10, 12)
+pole_game.init()
